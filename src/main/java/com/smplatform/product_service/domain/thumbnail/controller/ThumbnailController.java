7b3f73b7189c654f034ff096d2208f2fee5587d3
qaw <osshun21@gmail.com>
2025-03-01 14:12:08 +0900

    feat : swagger 내용 추가

--- src/main/java/com/smplatform/product_service/domain/thumbnail/controller/ThumbnailController.java
+++ src/main/java/com/smplatform/product_service/domain/thumbnail/controller/ThumbnailController.java
@@ -3,6 +3,8 @@ package com.smplatform.product_service.domain.thumbnail.controller;
 import com.smplatform.product_service.domain.thumbnail.dto.ThumbnailRequestDto;
 import com.smplatform.product_service.domain.thumbnail.dto.ThumbnailResponseDto;
 import com.smplatform.product_service.domain.thumbnail.service.ThumbnailService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -13,15 +15,18 @@ import java.util.List;
 @RestController
 @RequestMapping("/v1/thumbnails")
 @RequiredArgsConstructor
+@Tag(name = "Thumbnail", description = "Thumbnail management APIs")
 public class ThumbnailController {
     private final ThumbnailService thumbnailService;
 
     @GetMapping
+    @Operation(summary = "상품 썸네일 목록 조회", description = "전체 썸네일 저장 url 목록 조회")
     public ResponseEntity<List<ThumbnailResponseDto.ThumbnailInfo>> getProductThumbnailList(@RequestParam(required = false) Integer productId) {
         return ResponseEntity.ok(thumbnailService.getProductThumbnailList(productId));
     }
 
     @PostMapping
+    @Operation(summary = "상품 썸네일 목록 저장", description = "상품에 등록된 썸네일 url 목록 저장")
     public ResponseEntity<String> saveThumbnail(@Valid @RequestBody ThumbnailRequestDto.SaveThumbnail requestBody) {
         thumbnailService.saveThumbnail(requestBody.getProductId(), requestBody.getPaths());
         return ResponseEntity.status(201).body("save successful");
@@ -29,6 +34,7 @@ public class ThumbnailController {
 
 
     @PostMapping("/delete-thumbnail")
+    @Operation(summary = "특정 썸네일 삭제", description = "하나의 썸네일 삭제")
     public ResponseEntity<String> deleteThumbnail(@Valid @RequestBody ThumbnailRequestDto.DeleteThumbnail requestBody) {
         thumbnailService.deleteThumbnail(requestBody.getThumbnailId());
         return ResponseEntity.status(204).build();
