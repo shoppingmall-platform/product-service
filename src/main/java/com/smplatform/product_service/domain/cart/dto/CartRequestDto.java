0c6b4f0fa50185413799c2bb34d90d1ecfb95fe5
codeTheStudent <hjun303@gnu.ac.kr>
2025-05-11 06:22:09 +0900

    feat : cart 컨트롤러 구현, swagger api 명세 작성 완료

---
+++ src/main/java/com/smplatform/product_service/domain/cart/dto/CartRequestDto.java
@@ -0,0 +1,17 @@
+package com.smplatform.product_service.domain.cart.dto;
+
+import lombok.Getter;
+
+public class CartRequestDto {
+
+    @Getter
+    public static class CartAdd {
+        private Long productOptionId;
+        private Integer quantity;
+    }
+
+    @Getter
+    public static class CartUpdate {
+        private Integer quantity;
+    }
+}
