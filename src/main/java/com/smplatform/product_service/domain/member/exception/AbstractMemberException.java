6ce287ddbbd3ab4559cce94cbf91c4f67aa30cf0
qaw <osshun21@gmail.com>
2025-05-23 19:01:55 +0900

    fix: api 설계대로 전체적으로 수정 완료

--- src/main/java/com/smplatform/product_service/domain/member/exception/AbstractMemberException.java
+++ src/main/java/com/smplatform/product_service/domain/member/exception/AbstractMemberException.java
@@ -2,8 +2,8 @@ package com.smplatform.product_service.domain.member.exception;
 
 import org.springframework.http.HttpStatus;
 
-public abstract class AbstractApiException extends RuntimeException {
-    protected AbstractApiException(String message) {
+public abstract class AbstractMemberException extends RuntimeException {
+    protected AbstractMemberException(String message) {
         super(message);
     }
 
