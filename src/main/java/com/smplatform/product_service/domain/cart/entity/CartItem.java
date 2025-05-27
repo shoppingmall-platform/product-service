7832d7e14c40ddaa5dfa860e7744d2c5f3e8d5c3
qaw <osshun21@gmail.com>
2025-05-27 20:45:11 +0900

    feat: cart entity 수정

--- src/main/java/com/smplatform/product_service/domain/cart/entity/CartItem.java
+++ src/main/java/com/smplatform/product_service/domain/cart/entity/CartItem.java
@@ -3,9 +3,12 @@ package com.smplatform.product_service.domain.cart.entity;
 import com.smplatform.product_service.domain.member.entity.Member;
 import com.smplatform.product_service.domain.product.entity.ProductOption;
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "cartItems")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class CartItem {
     @Id
     @Column(name = "cart_item_id")
@@ -19,4 +22,20 @@ public class CartItem {
     @ManyToOne
     @JoinColumn(name = "product_option_id")
     private ProductOption productOption;
+
+    @Column
+    private int quantity;
+
+    private CartItem(Member member, ProductOption productOption) {
+        this.member = member;
+        this.productOption = productOption;
+    }
+
+    public static CartItem createCartItem(Member member, ProductOption productOption) {
+        return new CartItem(member, productOption);
+    }
+
+    public void updateProductOption(ProductOption productOption) {
+        this.productOption = productOption;
+    }
 }
