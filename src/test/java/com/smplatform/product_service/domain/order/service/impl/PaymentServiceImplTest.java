39ca8fee47cdfeca1f99bb99a3c0d28a908f7c8c
qaw302 <osshun21@gmail.com>
2026-01-08 11:43:34 +0900

    fix: 미완성 테스트 주석

--- src/test/java/com/smplatform/product_service/domain/order/service/impl/PaymentServiceImplTest.java
+++ src/test/java/com/smplatform/product_service/domain/order/service/impl/PaymentServiceImplTest.java
@@ -13,18 +13,18 @@ class PaymentServiceImplTest {
     @Autowired
     private PaymentServiceImpl paymentService;
 
-    @Test
-    void 실결제_승인API_호출_성공() {
-        // 🔐 실제 결제 완료 후 리디렉션에서 얻은 값을 아래에 대입하세요
-        PaymentRequestDto.PaymentConfirm dto = new PaymentRequestDto.PaymentConfirm();
-        dto.setPaymentKey("pay_abc123"); // 실제 결제 완료 후 받은 키
-        dto.setOrderId("order_123456");  // 결제 시 사용한 주문 ID
-        dto.setAmount(10000L);           // 결제 금액 (일치해야 함)
-
-        String result = paymentService.confirmPayment(dto);
-        System.out.println("응답 결과: " + result);
-
-        assertTrue(result.contains("DONE")); // 성공 응답 시 DONE 포함
-    }
+//    @Test
+//    void 실결제_승인API_호출_성공() {
+//        // 🔐 실제 결제 완료 후 리디렉션에서 얻은 값을 아래에 대입하세요
+//        PaymentRequestDto.PaymentConfirm dto = new PaymentRequestDto.PaymentConfirm();
+//        dto.setPaymentKey("pay_abc123"); // 실제 결제 완료 후 받은 키
+//        dto.setOrderId("order_123456");  // 결제 시 사용한 주문 ID
+//        dto.setAmount(10000L);           // 결제 금액 (일치해야 함)
+//
+//        String result = paymentService.confirmPayment(dto);
+//        System.out.println("응답 결과: " + result);
+//
+//        assertTrue(result.contains("DONE")); // 성공 응답 시 DONE 포함
+//    }
 
 }
